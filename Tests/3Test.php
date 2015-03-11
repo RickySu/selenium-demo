2e4152c0cfdcf4ff674fe55558b86e2eb710aff8
Ricky Su <ricky@ez2.us>
2015-03-11 15:00:07 +0800

    update

--- Tests/3Test.php
+++ Tests/3Test.php
@@ -15,18 +15,24 @@ class clickTest extends \PHPUnit_Extensions_Selenium2TestCase
                  'port' => 4444,
                  'browser' => 'chrome test browser',
                  'browserName' => 'chrome', 
+            ),
+            array(
+                 'host' => '127.0.0.1',
+                 'port' => 4445,
+                 'browser' => 'ie test browser',
+                 'browserName' => 'ie', 
             ),            
         );        
     }
     
     public function setup()
     {
-        $this->setBrowserUrl('http://getbootstrap.com/');
+        $this->setBrowserUrl('http://www.google.com/');
     }    
     
     public function test_click()
     {
-        $this->url('/javascript/');
+        $this->url('/');
         $testImage = new imagick();
         $testImage->readimageblob($this->currentScreenshot());
         $testImage->writeimage(__DIR__."/screenshot/{$this->getBrowser()}.jpg");
