749f999a69f58b6d6af9b629447a4b09a4f4c82e
Ricky Su <ricky@ez2.us>
2015-03-11 14:49:18 +0800

    add example

---
+++ Tests/0Test.php
@@ -0,0 +1,72 @@
+<?php
+class clickTest extends \PHPUnit_Extensions_Selenium2TestCase
+{
+    public static function browsers()
+    {
+        return array(
+            array(
+                 'host' => '127.0.0.1',
+                 'port' => 4444,
+                 'browser' => 'firefox test browser',
+                 'browserName' => 'firefox', 
+            ),
+            array(
+                 'host' => '127.0.0.1',
+                 'port' => 4444,
+                 'browser' => 'chrome test browser',
+                 'browserName' => 'chrome', 
+            ),
+            array(
+                 'host' => '127.0.0.1',
+                 'port' => 4445,
+                 'browser' => 'ie test browser',
+                 'browserName' => 'ie', 
+            ),            
+        );        
+    }
+    
+    public function setup()
+    {
+        $this->setBrowserUrl('http://google.com/');
+    }
+
+    public function test_search_click()
+    {
+        $this->url('/');        
+
+        $this->byName('q')->value('Selenium');
+        $element = $this->byCssSelector('input[value="Google 搜尋"]');
+        $this->moveto($element);
+        $element->click();
+        $this->waitAjaxComplete();
+        $element = $this->byCssSelector('#rso .srg >li.g:first-child h3 a');
+        $this->assertEquals('Selenium - Web Browser Automation', $element->text());
+        $this->assertEquals('http://www.seleniumhq.org/', $element->attribute('href'));
+    }
+    
+    public function test_search_enter()
+    {
+        $this->url('/');        
+
+/*        $this->byName('q')->value('Selenium');
+        $element = $this->byCssSelector('input[value="Google 搜尋"]');
+        $this->moveto($element);
+        $element->click();
+        */
+        $this->byName('q')->value('Selenium');
+        $this->keys(PHPUnit_Extensions_Selenium2TestCase_Keys::ENTER);
+        $this->waitAjaxComplete();
+        $element = $this->byCssSelector('#rso .srg >li.g:first-child h3 a');
+        $this->assertEquals('Selenium - Web Browser Automation', $element->text());
+        $this->assertEquals('http://www.seleniumhq.org/', $element->attribute('href'));
+    }
+    
+    public function waitAjaxComplete()
+    {
+        $this->waitUntil(function(){
+            if($this->byCssSelector('#rso')->displayed()){
+                return true;
+            }
+        }, 10000);
+    }
+}
---
+++ Tests/4Test.php
@@ -0,0 +1,43 @@
+<?php
+class clickTest extends \PHPUnit_Extensions_Selenium2TestCase
+{
+    public static function browsers()
+    {
+        return array(
+            array(
+                 'host' => '127.0.0.1',
+                 'port' => 4444,
+                 'browser' => 'firefox test browser',
+                 'browserName' => 'firefox', 
+            ),
+            array(
+                 'host' => '127.0.0.1',
+                 'port' => 4444,
+                 'browser' => 'chrome test browser',
+                 'browserName' => 'chrome', 
+            ),            
+        );        
+    }
+    
+    public function setup()
+    {
+        $this->setBrowserUrl('http://play.niceday.tw/');
+    }    
+        
+    public function test_max()
+    {
+        $this->currentWindow()->maximize();
+        $this->url('/');
+        sleep(1);
+        $this->assertTrue($this->byCssSelector("#homeBanner")->displayed());
+    }
+  
+    public function test_monile()
+    {
+        $this->currentWindow()->size(array('width' => 400, 'height' => 600));    
+        $this->url('/');    
+        sleep(1);
+        $this->assertFalse($this->byCssSelector("#homeBanner")->displayed());        
+    }    
+
+}
